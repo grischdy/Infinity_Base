cc882b94d7186416b1862ee3f662ba812666c6e0
Christian Schroem <Christian.Schroem@pa-ats.com>
2021-09-16 18:03:29 +0200

    Revert "AML erfolgreich importiert, Upload erfolgreich importiert,"
    This reverts commit 995f1e3eb9411dfea179a655a933b2e4b7628a36.

---
+++ Infinity_Base/AML Funktionen/Blob_Folder_Management.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace Infinity_Base.AML_Funktionen
+{
+    public class Blob_Folder_Management
+    {
+
+    }
+}
